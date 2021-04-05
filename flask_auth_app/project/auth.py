2e1a2f20f816fdfd162d0591001f4b67f87eac24
michodgs25 <michodgs@gmail.com>
2021-04-05 11:00:40 +0000

    Update auth.py authentication filter

--- flask_auth_app/project/auth.py
+++ flask_auth_app/project/auth.py
@@ -1,19 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
-auth = Blueprint('auth', __name__)
 
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
 
-@auth.route('/login')
-def login():
-    return render_template('login.html')
+    user = User.query.filter_by(email=email).first()
+    # if this returns a user, then the email already exists in database
 
+    if user:
+        # if a user is found,
+        # we want to redirect back to signup page so user can try again
+        return redirect(url_for('auth.signup'))
 
-@auth.route('/signup')
-def signup():
-    render_template('signup.html')
+    # create a new user with the form data.
+    # Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name,
+                    password=generate_password_hash(
+                        password, method='sha256'))
 
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
 
-@auth.route('/logout')
-def logout():
-    return 'logout'
+    return redirect(url_for('auth.login'))
+
