1e8188ec2dd5cc41064de087f6ccf0f6e33d3ab0
michodgs25 <michodgs@gmail.com>
2021-04-05 13:59:17 +0000

    Add user login check

--- flask_auth_app/project/auth.py
+++ flask_auth_app/project/auth.py
@@ -1,7 +1,7 @@
 from flask import (Blueprint, render_template,
                    redirect, url_for, request, flash)
 from werkzeug.security import (generate_password_hash,
-                               check_password_hash)
+                               check_password_hash, auth)
 from .models import User
 from . import db
 
@@ -32,3 +32,26 @@ def signup_post():
     db.session.commit()
 
     return redirect(url_for('auth.login'))
+
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password,
+    # hash it,and compare it
+    # to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+        # if the user doesn't exist or password is wrong,
+        # reload the page
+
+    # if the above check passes,
+    # then we know the user has the right credentials
+    return redirect(url_for('main.profile'))
