45f3fc1e4b916eb86669ccf180d1400812e6cc7e
michodgs25 <michodgs@gmail.com>
2021-04-05 15:13:15 +0000

    Format init.py code

--- flask_auth_app/project/__init__.py
+++ flask_auth_app/project/__init__.py
@@ -25,9 +25,9 @@ def create_app():
 
     return app
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of,
-        # our user table, use it in the query for the user
-        return User.query.get(int(user_id))
 
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of,
+    # our user table, use it in the query for the user
+    return User.query.get(int(user_id))
